07fbc468308fc0209255aee5c78ffdf0b6ad0514
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-05-16 13:47:38 -0500

    second or third

--- Menu.cs
+++ Menu.cs
@@ -1,10 +1,8 @@
-
-
 namespace Ch04Studio
 {
     public class Menu
     {
-        private List<MenuItem> MenuItems { get; } = [];
-        private DateTime lastUpdated {get; set; } = System.DateTime.Now;
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+        public DateTime LastUpdated {get; set; } = System.DateTime.Now;
     }
 }
--- MenuItem.cs
+++ MenuItem.cs
@@ -2,17 +2,18 @@ namespace Ch04Studio
 {
     public class MenuItem
     {
+        public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
-        public bool IsNew { get; set; } = true;
+        public DateTime DateAdded { get; set; }
         
-        public MenuItem(double price, string description, string category, bool isNew)
+        public MenuItem(string name, double price, string description, string category)
         {
+            Name = name;
             Price = price;
             Description = description;
             Category = category;
-            IsNew = isNew;
         }
         
     }
